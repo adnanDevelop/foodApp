a5eee328c3f04af83e739563dbcca94256e02c6a
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-07-02 20:02:20 +0500

    creating food card

--- frontend/src/modules/home/component/Restaurant.jsx
+++ frontend/src/modules/home/component/Restaurant.jsx
@@ -7,18 +7,19 @@ const Restaurant = () => {
         <SectionHeader title="Featured Restaurant" />
 
         {/* Cards */}
-        <section className="mt-[40px] grid grid-cols-12">
-          <div className="w-full col-span-3 p-3 border border-gray-300 rounded-md food_card">
+        <section className="mt-[40px] grid grid-cols-12 gap-4">
+          <div className="w-full col-span-3 p-3 border border-gray-300 rounded-md food_card tarnsitions hover:shadow-md group">
             {/* Image */}
-            <div className="relative rounded-md">
+            <div className="relative overflow-hidden rounded-md">
               <img
                 src="/image/restaurant/img-1.png"
-                className="rounded-md"
+                className="transition rounded-md group-hover:scale-105"
                 alt=""
               />
+              <div className="absolute bottom-0 left-0 w-full h-[40%]  bg-gradient-to-t from-[rgba(0,0,0,1)] via-[rgba(0,0,0,0.6)] to-[rgba(255,255,255,0)]" />
               {/* label */}
-              <div className="absolute bottom-2 left-2">
-                <p className="text-xs font-normal text-white">upto $2</p>
+              <div className="absolute bottom-2 left-3">
+                <p className="mb-1 text-xs font-normal text-white">Up to $2</p>
                 <h4 className="text-sm font-semibold text-white">50% OFF</h4>
               </div>
             </div>
